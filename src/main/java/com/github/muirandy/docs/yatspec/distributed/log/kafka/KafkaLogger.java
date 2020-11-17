438d657ac367db3fc616640cb400f79b0be0cd2a
Andy Muir <muirandy@yahoo.co.uk>
2020-11-17 18:03:53 +0000

    Allow Log(s) that have been logged to be retrieved! In memory atm. Nothing forcing anything better yet!

--- src/main/java/com/github/muirandy/docs/yatspec/distributed/log/kafka/KafkaLogger.java
+++ src/main/java/com/github/muirandy/docs/yatspec/distributed/log/kafka/KafkaLogger.java
@@ -5,16 +5,18 @@ import com.github.muirandy.docs.yatspec.distributed.Log;
 import com.github.muirandy.docs.yatspec.distributed.Logs;
 
 public class KafkaLogger implements DiagramLogger {
+    private Logs logs = new Logs();
+
     public KafkaLogger(String kafkaHost, Integer kafkaPort, String topicName) {
     }
 
     @Override
     public void log(Log log) {
-
+        logs.add(log);
     }
 
     @Override
     public Logs read() {
-        return new Logs();
+        return logs;
     }
 }
--- src/test/java/com/github/muirandy/docs/yatspec/distributed/log/kafka/KafkaLoggerShould.java
+++ src/test/java/com/github/muirandy/docs/yatspec/distributed/log/kafka/KafkaLoggerShould.java
@@ -1,7 +1,10 @@
 package com.github.muirandy.docs.yatspec.distributed.log.kafka;
 
 import com.github.muirandy.docs.yatspec.distributed.DiagramLogger;
+import com.github.muirandy.docs.yatspec.distributed.Log;
 import com.github.muirandy.docs.yatspec.distributed.Logs;
+import org.junit.Before;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.testcontainers.containers.KafkaContainer;
 import org.testcontainers.junit.jupiter.Container;
@@ -14,21 +17,36 @@ import static org.assertj.core.api.Assertions.assertThat;
 class KafkaLoggerShould {
 
     private static final String TOPIC_NAME = "living-documentation";
+    private static final String LOG_MESSAGE = "Message";
+    private static final Object BODY = "Body";
 
     @Container
     private final KafkaContainer kafka =
             new KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka:5.4.3"))
                 ;
 
-    
-    @Test
-    void returnEmptyLogs() {
+    DiagramLogger kafkaLogger;
+    private Log log = new Log(LOG_MESSAGE, BODY);
+
+    @BeforeEach
+    void setUp() {
         String kafkaHost = kafka.getHost();
         Integer kafkaPort = kafka.getMappedPort(9092);
+        kafkaLogger = new KafkaLogger(kafkaHost, kafkaPort, TOPIC_NAME);
+    }
 
-        DiagramLogger kafkaLogger = new KafkaLogger(kafkaHost, kafkaPort, TOPIC_NAME);
+    @Test
+    void returnEmptyLogs() {
         Logs logs = kafkaLogger.read();
 
         assertThat(logs.getLogs()).isEmpty();
+    }
+
+    @Test
+    void retrieveLogs() {
+        kafkaLogger.log(log);
+        Logs logs = kafkaLogger.read();
+
+        assertThat(logs.getLogs()).containsExactly(log);
     }
 }
